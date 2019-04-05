8d6cd30f6193607935e3db910ef07de349f3cdaa
Роман Кузнецкий <r.kuzneckiy@geoscan.lan>
2019-04-05 15:40:35 +0300

    conf py image align H

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Geoscan 201'
+project = 'Geoscan 2019'
 copyright = 'Geoscan 2019'
 author = ''
 
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
